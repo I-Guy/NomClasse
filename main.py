5b1fb87361356b11a72bd286c022ed673415546a
ilango <guy.ilango@univ-tours.fr>
2022-10-12 15:52:06 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+__author__ = "Ilango Guy"
+__version__ = "0.0.1"
+__licence__ = "CEPR"
+__doc__ = "Main menu for labtools of cepr"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
